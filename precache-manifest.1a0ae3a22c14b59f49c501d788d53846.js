95a39b42b6d9b9ff1be51c50a6290ea314b106b6
Gabriel Ferreira <gabrielferrado@Gabriels-iMac.local>
2021-02-05 15:12:56 -0300

    Create project

---
+++ precache-manifest.1a0ae3a22c14b59f49c501d788d53846.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c10cebc803aa69d46522dc6fbb26251",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be8714c35f0259f799e8",
+    "url": "/static/css/main.eea256fd.chunk.css"
+  },
+  {
+    "revision": "b99ab9edc7c35544a8f3",
+    "url": "/static/js/2.cdfca677.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.cdfca677.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be8714c35f0259f799e8",
+    "url": "/static/js/main.823db6d6.chunk.js"
+  },
+  {
+    "revision": "f13c66090e57de6f2e62",
+    "url": "/static/js/runtime-main.d0252265.js"
+  },
+  {
+    "revision": "a8ece86e0373fa3e5797dce37ad46bd1",
+    "url": "/static/media/doctors.a8ece86e.svg"
+  },
+  {
+    "revision": "9df168ab44f9be01a7654e934221be54",
+    "url": "/static/media/integration.9df168ab.svg"
+  },
+  {
+    "revision": "bcbb0122c1959f97d7f58ddd4e03379e",
+    "url": "/static/media/logo.bcbb0122.png"
+  },
+  {
+    "revision": "363aa3356d363e8b932a9e1f7c24aab5",
+    "url": "/static/media/logo_white.363aa335.png"
+  },
+  {
+    "revision": "bf19cd2a2f147d449cd0e9ccb9144247",
+    "url": "/static/media/online_reports.bf19cd2a.svg"
+  },
+  {
+    "revision": "c00b79acc18e64adb2bf1652073c55f2",
+    "url": "/static/media/reports.c00b79ac.svg"
+  }
+]);
